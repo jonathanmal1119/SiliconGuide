10660a5db1b39847556e0283526b143e140e7c8d
Richie Rojas <rrojas181920@gmail.com>
2025-07-26 14:26:12 -0400

    Updated SavedList to stay white when clicked.
    Also made the whole rendering process more modular by putting in a list and using a map to show it.

--- client/src/components/SavedList.jsx
+++ client/src/components/SavedList.jsx
@@ -1,20 +1,15 @@
 import './styles/SavedList.css';
 
-function SavedList(meta_data) {
-    const {
-        name,
-        isLast
-    } = meta_data
-
+function SavedList({ name, isLast, selected, onClick }) {
     return (
-        <div className="saved-list">
-            <div className="tree-item">
-                <div className={`tree-leaf ${isLast ? "last-branch" : "mid-branch"}`}>
-                    {name}
-                </div>
-            </div>
+    <div className="saved-list">
+      <div className={`tree-item ${selected ? 'selected' : ''}`} onClick={onClick}>
+        <div className={`tree-leaf ${isLast ? "last-branch" : "mid-branch"}`}>
+          {name}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SavedList
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,16 +1,34 @@
 import './styles/Sidebar.css';
 import SavedList from './SavedList';
+import { useState } from 'react';
 
 function Sidebar() {
+
+    const savedLists = [
+    "Jon's List",
+    "Richies's List",
+    "Test List 2"
+    ];
+ const [selectedList, setSelectedList] = useState(savedLists[0]);
+
+
+
+
     return (
         <div className="sidebar-container">
             <span className='Title'>Saved Lists</span>
             <div className='spacer-tree'/>
             <div className="list-tree">
                 <div className='tree-spacer'/>
-                <SavedList name="Jon's List"/>
-                <SavedList name="Richies's List"/>
-                <SavedList name="Test List 2" isLast/>
+                {savedLists.map((name, index) => (
+                <SavedList
+                    key={name}
+                    name={name}
+                    isLast={index === savedLists.length - 1}
+                    selected={selectedList === name}
+                    onClick={() => setSelectedList(name)}
+                />
+                ))}
             </div>
         </div>
     );
