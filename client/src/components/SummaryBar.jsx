005f750131524c2c94f147f1f59ba4a1e88f29af
Jonathan Malave <jmaxmalave@aol.com>
2025-07-26 14:01:13 -0400

    SummaryBar Styling

--- client/src/components/SummaryBar.jsx
+++ client/src/components/SummaryBar.jsx
@@ -1,12 +1,22 @@
 import './styles/Summarybar.css';
+import SummaryItem from './SummaryItem';
 
 function SummaryBar() {
+
+
     return (
         <div className="summarybar-container">
-            <h2>Parts Summary</h2>
-            <table className="parts-table">
-
-            </table>
+            <span className="summary-title">Parts Summary</span>
+            <div className='summary-list'>
+                <SummaryItem category_name="CPU" part_name="AMD Ryzen 5 7600X"/>
+                <SummaryItem category_name="Motherboard" part_name="MSI B650M PRO-VDH WIFI"/>
+                <SummaryItem category_name="Memory" part_name="Corsair Vengeance 16GB (2x8GB) DDR5-6000"/>
+                <SummaryItem category_name="Storage" part_name="Crucial P5 Plus 1TB NVMe SSD"/>
+                <SummaryItem category_name="Graphics Card" part_name="NVIDIA GeForce RTX 4070 12GB"/>
+                <SummaryItem category_name="Power Supply" part_name="Corsair RM750e 750W 80+ Gold"/>
+                <SummaryItem category_name="CPU Cooler" part_name="NZXT H5 Flow ATX Mid Tower"/>
+                <SummaryItem category_name="Case Fans" part_name="N/A"/>
+            </div>
         </div>
     );
 };
---
+++ client/src/components/SummaryItem.jsx
@@ -0,0 +1,19 @@
+import './styles/SavedList.css';
+
+function SummaryItem(meta_data) {
+    const {
+        category_name,
+        part_name
+    } = meta_data
+
+    return (
+            <div className="summary-lea">
+                <div className='summary-text'>{category_name}</div>
+                <div className="summary-leaf last-branch">
+                    {part_name}
+                </div>
+            </div>
+    );
+};
+
+export default SummaryItem
