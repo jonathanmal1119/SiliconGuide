ef2b8868f8a6876b57c2f0a3ca38cdaf7b174751
Richie Rojas <rrojas181920@gmail.com>
2025-07-26 13:56:34 -0400

    PartsList was fixed to contain the actual default data for each header.
    Created a data folder that contains a defaultParts.js that contains all the default objects for mapping purposes.

---
+++ client/src/data/defaultParts.js
@@ -0,0 +1,20 @@
+const defaultParts = {
+        "CPU": [
+            { name: "Intel Core i9-13900K", ratings: 5.0, price: 429.99, wattage: 125, link:"https://www.amazon.com/Intel-i9-13900K-Desktop-Processor-P-cores/dp/B0BCF54SR1" },
+            { name: "AMD Ryzen 9 7950X3D", ratings: 2.1, price: 599.99, wattage: 120, link:"https://www.amazon.com/AMD-Ryzen-7950X3D-Hexadeca-core-Processor/dp/B0BTRH9MNS"},
+            { name: "Intel Core i7-13700K", ratings: 3.2, price: 349.99, wattage: 100,link:"https://www.amazon.com/Intel-i7-13700K-Desktop-Processor-P-cores/dp/B0BCF57FL5"},
+            { name: "AMD Ryzen 7 7700X", ratings: 4.4, price: 399.99, wattage: 85, link:"https://www.amazon.com/AMD-7700X-16-Thread-Unlocked-Processor/dp/B0BBHHT8LY?th=1" },
+            { name: "Intel Core i5-13600K", ratings: 3.9, price: 249.99, wattage: 65,link:"https://www.amazon.com/Intel-i5-13600K-Desktop-Processor-P-cores/dp/B0BCDR9M33" }
+        ],
+        "Motherboard": [],
+        "Memory": [],
+        "Storage": [],
+        "Graphics Card": [],
+        "Case": [],
+        "Power Supply": [],
+        "CPU Cooler": [],
+        "Case Fans": []
+        };
+
+
+export default defaultParts;
